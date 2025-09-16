dc1ab08a92ebeea8e981917d13776a3ed6cf02cd
Kris <kris_s2@outlook.com>
2025-09-15 21:58:42 -0700

    kata: decided not to support PATCH operations anymore. Moving to use STJ and full PUT support.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,4 +1,31 @@
 ﻿
 var builder = WebApplication.CreateBuilder(args);
 
-//Adding Services.
+// Adding Services.
+builder.Services.AddCors(options => {
+	options.AddPolicy("AllowOrigin", 
+		builder => builder.AllowAnyOrigin());
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options => 
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null; // Keeps PascalCase
+        options.JsonSerializerOptions.WriteIndented = true; // Pretty print (optional)
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipliene
+if (app.Environment.IsDevelopment())
+	app.UseDeveloperExceptionPage();
+else
+	app.UseExceptionHandler("/Error");
+
+app.UseStaticFiles();
+app.UseStatusCodePages();
+
+app.UseCors("AllowOrigin");
+
+app.UseRouting();
+app.MapControllers();
