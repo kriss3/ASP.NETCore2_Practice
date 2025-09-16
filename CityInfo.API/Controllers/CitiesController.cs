f22768194f571500b511ecf9be96397cb5fe0a75
Kris <kris_s2@outlook.com>
2025-09-15 22:04:35 -0700

    kata: cleanup and file based namesapces.

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,31 +1,26 @@
 ﻿using CityInfo.API.Store;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace CityInfo.API.Controllers
+namespace CityInfo.API.Controllers;
+
+[Route("/api/cities")]
+[EnableCors("AllowOrigin")]
+public class CitiesController : Controller
 {
-    [Route("/api/cities")]
-    [EnableCors("AllowOrigin")]
-    public class CitiesController : Controller
+    [HttpGet()]
+    public IActionResult GetCities()
     {
-        [HttpGet()]
-        public IActionResult GetCities()
-        {
-            return Ok(CitiesDataStore.Current.Cities);
-        }
+        return Ok(CitiesDataStore.Current.Cities);
+    }
 
-        [HttpGet("{id}")]
-        public IActionResult GetCity(int id)
-        {
-            //find city;
-            var foundCity = CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id == id);
-            if (foundCity == null)
-                return NotFound();
-            return Ok(foundCity);
-        }
+    [HttpGet("{id}")]
+    public IActionResult GetCity(int id)
+    {
+        //find city;
+        var foundCity = CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id == id);
+        if (foundCity == null)
+            return NotFound();
+        return Ok(foundCity);
     }
 }
