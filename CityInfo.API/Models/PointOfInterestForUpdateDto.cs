3e8ddf62e2c20decff6d0c6e4bb92e8b974f1ebd
Kris <kris_s2@outlook.com>
2025-09-15 22:07:56 -0700

    kata: attempt to support nullable in this project.

--- CityInfo.API/Models/PointOfInterestForUpdateDto.cs
+++ CityInfo.API/Models/PointOfInterestForUpdateDto.cs
@@ -1,13 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace CityInfo.API.Models
+namespace CityInfo.API.Models;
+
+public class PointOfInterestForUpdateDto
 {
-    public class PointOfInterestForUpdateDto
-    {
-        [Required(ErrorMessage = "You should provide a name value.")]
-        [MaxLength(50)]
-        public string Name { get; set; }
-        [MaxLength(200)]
-        public string Description { get; set; }
-    }
+	[Required(ErrorMessage = "You should provide a name value.")]
+	[MaxLength(50)]
+	public string Name { get; set; } = string.Empty;
+
+	[MaxLength(200)]
+	public string Description { get; set; } = string.Empty;
 }
