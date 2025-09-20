265f88f1771dd0b0b04d6d3eeda6b71cca983318
Kris <kris_s2@outlook.com>
2025-09-20 16:03:05 -0700

    kata: adding sinatures for the interface functions.

--- CityInfo.API/Application/DataTransportObjcts.cs
+++ CityInfo.API/Application/DataTransportObjcts.cs
@@ -1,13 +1,12 @@
 ﻿namespace CityInfo.API.Application;
 
-public record CitySummeryDto();
+public record CitySummeryDto(int Id, string Name, string? Description);
 
-public record CityDto();
+public record CityDto(int Id, string Name, string? Description, IReadOnlyList<PointOfInterestDto> PointsOfInterest);
 
-public record PointOfInterestDto();
+public record PointOfInterestDto(int Id, string Name, string? Description);
 
-public record CreatePointOfInterestDto();
+public record CreatePointOfInterestDto(string Name, string? Description);
 
-public record UpdatePointOfInterestDto();
-
+public record UpdatePointOfInterestDto(string Name, string? Description);
 
