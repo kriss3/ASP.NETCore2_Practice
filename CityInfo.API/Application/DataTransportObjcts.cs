3b2983e0c9cb4f6b1ac2786300a919a34518bac6
Kris <kris_s2@outlook.com>
2025-09-20 16:00:07 -0700

    kata: adding a scaffolding for DataTransport objects fro City and City's points of interests.

---
+++ CityInfo.API/Application/DataTransportObjcts.cs
@@ -0,0 +1,13 @@
+﻿namespace CityInfo.API.Application;
+
+public record CitySummeryDto();
+
+public record CityDto();
+
+public record PointOfInterestDto();
+
+public record CreatePointOfInterestDto();
+
+public record UpdatePointOfInterestDto();
+
+
