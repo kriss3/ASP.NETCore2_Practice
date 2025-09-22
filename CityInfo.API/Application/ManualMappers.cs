ae889754e4ef0bb3847fbcd08f1c103932d99d4d
Kris <kris_s2@outlook.com>
2025-09-21 23:44:41 -0700

    kata: adding mapper for entity to dto

--- CityInfo.API/Application/ManualMappers.cs
+++ CityInfo.API/Application/ManualMappers.cs
@@ -4,6 +4,10 @@ namespace CityInfo.API.Application;
 
 public static class ManualMappers
 {
+
+	public static PointOfInterestDto ToDto(this PointOfInterest p) =>
+		new(p.Id, p.Name, p.Description);
+
 	public static void Apply(this UpdatePointOfInterestDto dto, PointOfInterest entity)
 	{
 		entity.Name = dto.Name;
