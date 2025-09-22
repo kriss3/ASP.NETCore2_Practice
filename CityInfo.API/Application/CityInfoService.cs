7cd3abeec0c2cb1aa4a04da0f9350de0e3f9e957
Kris <kris_s2@outlook.com>
2025-09-22 00:00:16 -0700

    kata: adding cityinfo stab method.

---
+++ CityInfo.API/Application/CityInfoService.cs
@@ -0,0 +1,30 @@
+﻿
+namespace CityInfo.API.Application;
+
+public class CityInfoService : ICityInfoService
+{
+	public Task<PointOfInterestDto?> AddPointOfInterestAsync(int cityId, CreatePointOfInterestDto input, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> DeletePointOfInterestAsync(int cityId, int pointOfInterestId, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IReadOnlyList<CitySummaryDto>> GetCitiesAsync(CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CityDto?> GetCityAsync(int cityId, bool includePointsOfInterest, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> UpdatePointOfInterestAsync(int cityId, int pointOfInterestId, UpdatePointOfInterestDto input, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+}
