731251c9fa2d9b1dcfb940496a472fa470a5b762
Kris <kris_s2@outlook.com>
2025-09-23 00:04:22 -0700

    kata: adding interface contract

--- CityInfo.API/Application/ICityInfoRepository.cs
+++ CityInfo.API/Application/ICityInfoRepository.cs
@@ -1,5 +1,15 @@
-﻿namespace CityInfo.API.Application;
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Application;
 
 public interface ICityInfoRepository
 {
+	Task<IReadOnlyList<City>> GetCitiesAsync(CancellationToken cancellationToken);
+	Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest, CancellationToken cancellationToken);
+
+	Task<PointOfInterest?> GetPointAsync(int cityId, int pointId, CancellationToken cancellationToken);
+	Task AddPointAsync(City city, PointOfInterest point, CancellationToken ccancellationTokent);
+	void DeletePoint(PointOfInterest point);
+
+	Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
