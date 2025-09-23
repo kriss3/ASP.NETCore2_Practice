b944321320f4cbef9fbe9e6e60752d7e28888eb9
Kris <kris_s2@outlook.com>
2025-09-23 00:03:46 -0700

    kata: adding repository

--- CityInfo.API/Application/CityInfoService.cs
+++ CityInfo.API/Application/CityInfoService.cs
@@ -3,10 +3,13 @@ namespace CityInfo.API.Application;
 
 public class CityInfoService : ICityInfoService
 {
-	public Task<PointOfInterestDto?> AddPointOfInterestAsync(int cityId, CreatePointOfInterestDto input, CancellationToken cancellationToken)
+	private readonly ICityRepository _repo;
+	public CityInfoService(ICityRepository repo) => _repo = repo;
+
+	public async Task<PointOfInterestDto?> AddPointOfInterestAsync(int cityId, CreatePointOfInterestDto input, CancellationToken cancellationToken)
 	{
-		
-		
+		return await _repo.GetCitiesAsync(ct)).Select(c => c.ToSummaryDto()).ToList();
+
 	}
 
 	public Task<bool> DeletePointOfInterestAsync(int cityId, int pointOfInterestId, CancellationToken cancellationToken)
