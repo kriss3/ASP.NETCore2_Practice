e5153f4e4d719bc022b578d8ff662fa0499d9073
Kris <kris_s2@outlook.com>
2025-09-25 23:46:35 -0700

    kata: got mixed up with manual mappers. Keeping svc ext method type.

--- CityInfo.API/Application/ManualMappers.cs
+++ CityInfo.API/Application/ManualMappers.cs
@@ -2,18 +2,18 @@
 
 namespace CityInfo.API.Application;
 
-public static class ManualMappers
-{
-	public static CityDto ToDto(this City c) =>
-		new(c.Id, c.Name, c.Description,
-			[.. c.PointsOfInterest.Select(p => p.ToDto())]);
+//public static class ManualMappers
+//{
+//	public static CityDto ToDto(this City c) =>
+//		new(c.Id, c.Name, c.Description,
+//			[.. c.PointsOfInterest.Select(p => p.ToDto())]);
 
-	public static PointOfInterestDto ToDto(this PointOfInterest p) =>
-		new(p.Id, p.Name, p.Description);
+//	public static PointOfInterestDto ToDto(this PointOfInterest p) =>
+//		new(p.Id, p.Name, p.Description);
 
-	public static void Apply(this UpdatePointOfInterestDto dto, PointOfInterest entity)
-	{
-		entity.Name = dto.Name;
-		entity.Description = dto.Description ?? string.Empty;
-	}
-}
+//	public static void Apply(this UpdatePointOfInterestDto dto, PointOfInterest entity)
+//	{
+//		entity.Name = dto.Name;
+//		entity.Description = dto.Description ?? string.Empty;
+//	}
+//}
