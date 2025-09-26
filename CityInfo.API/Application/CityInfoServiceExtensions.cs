35ac17de6211034f4905397914a593329425e678
Kris <kris_s2@outlook.com>
2025-09-25 23:27:40 -0700

    kata: adding ToDto returning CtiyDto

--- CityInfo.API/Application/CityInfoServiceExtensions.cs
+++ CityInfo.API/Application/CityInfoServiceExtensions.cs
@@ -9,8 +9,12 @@ public static class CityInfoServiceExtensions
 		return new(c.Id, c.Name, c.Description);
 	}
 
-	public static CityDto ToDto(this City c) { }
-
+	public static CityDto ToDto(this City c) 
+	{
+		return new(c.Id, c.Name, c.Description,
+			(c.PointsOfInterest ?? new List<PointOfInterest>())
+				.Select(p => p.ToDto()).ToList());
+	}
 
 	public static PointOfInterestDto ToDto(this PointOfInterest p) { }
 
