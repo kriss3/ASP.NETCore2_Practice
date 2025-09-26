3a9739539362c5ea7509a7d98f841d0c9b85ab0e
Kris <kris_s2@outlook.com>
2025-09-25 23:25:47 -0700

    kata: adding stabs for extension methos.

--- CityInfo.API/Application/CityInfoServiceExtensions.cs
+++ CityInfo.API/Application/CityInfoServiceExtensions.cs
@@ -1,5 +1,18 @@
-﻿namespace CityInfo.API.Application;
+﻿using CityInfo.API.Entities;
 
-public class CityInfoServiceExtensions
+namespace CityInfo.API.Application;
+
+public static class CityInfoServiceExtensions
 {
+	public static CitySummaryDto ToSummaryDto(this City c) { }
+
+	public static CityDto ToDto(this City c) { }
+
+
+	public static PointOfInterestDto ToDto(this PointOfInterest p) { }
+
+	public static PointOfInterest ToEntity(this CreatePointOfInterestDto dto) { }
+
+	public static void Apply(this UpdatePointOfInterestDto dto, PointOfInterest entity) { }
 }
+
