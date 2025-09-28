18e381bb81c540e20c0a6295326832946e54b568
Kris <kris_s2@outlook.com>
2025-09-27 18:01:11 -0700

    kata: installing sqlserver for EF Core.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using CityInfo.API.Application;
 using CityInfo.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,12 @@ builder.Services.AddCors(options => {
 		.AllowAnyMethod()
 		.AllowAnyHeader());
 });
+
+
+builder.Services.AddDbContext<CityInfoContext>(options => {
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
 builder.Services.AddScoped<ICityInfoService, CityInfoService>();
