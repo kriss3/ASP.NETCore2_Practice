bebf318420f95a14b274a5b23f4239403880cd98
Kris <kris_s2@outlook.com>
2025-09-27 17:51:08 -0700

    kata: worging with DI to add ICityInfoServce interface and its implemention.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddCors(options => {
 });
 
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+builder.Services.AddScoped<ICityInfoService, CityInfoService>();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options => 
