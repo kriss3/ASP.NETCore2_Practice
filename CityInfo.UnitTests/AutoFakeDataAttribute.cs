4bd30b80bde33895a807b846eb0e7825dc3bdd9d
Kris <kris_s2@outlook.com>
2025-10-03 23:30:30 -0700

    kata: working on customization of my unit tests to be.

---
+++ CityInfo.UnitTests/AutoFakeDataAttribute.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture;
+using AutoFixture.AutoFakeItEasy;
+using AutoFixture.Xunit3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.UnitTests;
+public sealed class AutoFakeDataAttribute : AutoDataAttribute
+{
+	public AutoFakeDataAttribute() : base(() =>
+	{
+		var f = new Fixture().Customize(new AutoFakeItEasyCustomization
+		{
+			ConfigureMembers = true // auto-props filled on fakes/POCOs
+		});
+		return f;
+	})
+	{ }
+}
+
+public sealed class InlineAutoFakeDataAttribute : InlineAutoDataAttribute { }
