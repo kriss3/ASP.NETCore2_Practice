d216e3f51e0738d76c25404cb93d8c145c814f80
Kris <kris_s2@outlook.com>
2025-10-10 23:26:56 -0700

    kata: adding act on the setup configured.

--- CityInfo.UnitTests/CityInfoServiceTests.cs
+++ CityInfo.UnitTests/CityInfoServiceTests.cs
@@ -93,5 +93,8 @@ public class CityInfoServiceTests
 		A.CallTo(() => repo.SaveChangesAsync(A<CancellationToken>._))
 			.Returns(1);
 
+		// Act
+		var result = await service.AddCityAsync(createCityDto, CancellationToken.None);
+
 	}
 }
